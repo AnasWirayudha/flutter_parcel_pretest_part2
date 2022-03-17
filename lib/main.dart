cf61549e8bc7befcf759b5360877dcfc49d5cd15
AnasWirayudha <anaswirayudha01@gmail.com>
2022-03-17 12:32:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_parcel_app/app.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'ui/screens/screens.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
